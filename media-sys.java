fa67c1dcfe51964a39c942ce8c21d24b4ebf6a2a
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-08-13 00:52:35 -0300

    Create media-sys.java

---
+++ media-sys.java
@@ -0,0 +1,14 @@
+public class CalculoMedia {
+    public static void main(String[] args) {
+        // Definindo os três números
+        double numero1 = 10.5;
+        double numero2 = 8.2;
+        double numero3 = 5.7;
+        
+        // Calculando a média
+        double media = (numero1 + numero2 + numero3) / 3;
+        
+        // Exibindo o resultado
+        System.out.println("A média dos números é: " + media);
+    }
+}
